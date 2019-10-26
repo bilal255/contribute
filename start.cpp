c56c3f3ea5ad2d3cd946926dd3b8c186a382cd2c
prayantgupta15 <37060330+prayantgupta15@users.noreply.github.com>
2019-10-26 22:06:20 +0530

    Update start.cpp

--- start.cpp
+++ start.cpp
@@ -1,19 +1,25 @@
 //This program does the basic arithmetic operation
 //i.e. addition and subtraction
 #include <iostream>
+void sum(int a, int b)
+{
+    cout<<a+b;
+}
+void sub(int a,int b)
+{
+    cout<<a-b;
+}
 using namespace std;
 int main()
 {
     int a, b, sum, sub;
     cout << "Enter the two numbers you want to add: ";
     cin >> a >> b;
-    sum = (a + b);
-    cout << "The addition of " << a << " and " << b << "are: " << sum;
-    system ("cls"),
+    cout << "The addition of " << a << " and " << b << "are: " << sum(a,b);
+    system ("cls");
     cout << "Enter the two numbers you want to subtract: ";
     cin >> a >> b;
-    sub = (a - b);
-    cout << "The subtraction of " << a << " and " << b << "are: " << sub;
+    cout << "The subtraction of " << a << " and " << b << "are: " << sub(a,b);
 
     return 0;
 }
